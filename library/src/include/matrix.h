82dcf4ec1277eb9697f279b1849c7fba7f0580db
Nico Trost <nico.trost@amd.com>
2018-04-17 11:47:27 +0200

    context and mat descriptor filled

--- library/src/include/matrix.h
+++ library/src/include/matrix.h
@@ -5,8 +5,19 @@
 #ifndef ROCSPARSE_MATRIX_H_
 #define ROCSPARSE_MATRIX_H_
 
-typedef struct rocsparseMatDescr {
-    int todo;
-} rocsparseMatDescr;
+#include "rocsparse.h"
+
+struct rocsparseMatDescr
+{
+    // Constructor
+    rocsparseMatDescr();
+    // Destructor
+    ~rocsparseMatDescr();
+
+    // Matrix index base
+    rocsparseIndexBase_t base;
+    // Matrix type
+    rocsparseMatrixType_t type;
+};
 
 #endif // ROCSPARSE_MATRIX_H_
