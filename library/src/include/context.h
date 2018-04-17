048ed66d9371f8b49d2d0dab703e9d4be193d953
Nico Trost <nico.trost@amd.com>
2018-04-17 08:46:44 +0200

    rocsparseContext and rocsparse header
    [ROCm/rocSPARSEcommit: 9ef7b6b0310526b1704db517ed41d935602e3248]

---
+++ library/src/include/context.h
@@ -0,0 +1,35 @@
+#ifndef ROCSPARSE_CONTEXT_H_
+#define ROCSPARSE_CONTEXT_H_
+
+#include "rocsparse.h"
+
+#include <iostream>
+#include <fstream>
+#include <hip/hip_runtime_api.h>
+
+/*******************************************************************************
+ * \brief rocsparseContext is a structure holding the rocsparse library context.
+******************************************************************************/
+struct rocsparseContext
+{
+    rocsparseContext();
+    ~rocsparseContext();
+
+    // device id
+    int device;
+    // device properties
+    hipDeviceProp_t properties;
+    // stream
+    hipStream_t stream;
+    // pointer mode
+    rocsparsePointerMode_t pointer_mode;
+    // logging mode
+    rocsparseLayerMode_t layer_mode;
+
+    std::ofstream log_trace_ofs;
+    std::ofstream log_bench_ofs;
+    std::ostream *log_trace_os;
+    std::ostream *log_bench_os;
+};
+
+#endif // ROCSPARSE_CONTEXT_H_
