87d5b628c4bc943e3b191b71c60cb4df3c667e8a
Nico Trost <nico.trost@amd.com>
2018-04-18 14:58:39 +0200

    hip error handling

--- library/src/include/definitions.h
+++ library/src/include/definitions.h
@@ -14,13 +14,34 @@
  * thereby it can include top-level definitions included by all
  ******************************************************************************/
 
-#define THROW_IF_HIP_ERROR(INPUT_STATUS_FOR_CHECK)                         \
-    {                                                                      \
-        hipError_t TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK;          \
-        if(TMP_STATUS_FOR_CHECK != hipSuccess)                             \
-        {                                                                  \
-            throw get_rocsparse_status_for_hip_status(TMP_STATUS_FOR_CHECK); \
-        }                                                                  \
+#define RETURN_IF_HIP_ERROR(INPUT_STATUS_FOR_CHECK)                             \
+    {                                                                           \
+        hipError_t TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK;               \
+        if(TMP_STATUS_FOR_CHECK != hipSuccess)                                  \
+        {                                                                       \
+            return get_rocsparse_status_for_hip_status(TMP_STATUS_FOR_CHECK);   \
+        }                                                                       \
+    }
+#define THROW_IF_HIP_ERROR(INPUT_STATUS_FOR_CHECK)                              \
+    {                                                                           \
+        hipError_t TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK;               \
+        if(TMP_STATUS_FOR_CHECK != hipSuccess)                                  \
+        {                                                                       \
+            throw get_rocsparse_status_for_hip_status(TMP_STATUS_FOR_CHECK);    \
+        }                                                                       \
+    }
+
+#define PRINT_IF_HIP_ERROR(INPUT_STATUS_FOR_CHECK)                              \
+    {                                                                           \
+        hipError_t TMP_STATUS_FOR_CHECK = INPUT_STATUS_FOR_CHECK;               \
+        if(TMP_STATUS_FOR_CHECK != hipSuccess)                                  \
+        {                                                                       \
+            fprintf(stderr,                                                     \
+                    "hip error code: %d at %s:%d\n",                            \
+                    TMP_STATUS_FOR_CHECK,                                       \
+                    __FILE__,                                                   \
+                    __LINE__);                                                  \
+        }                                                                       \
     }
 
 #endif // ROCSPARSE_DEFINITIONS_H_
