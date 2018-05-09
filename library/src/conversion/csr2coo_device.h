151a27eb3e77f54a16a6d3dd25c22d930388d716
Nico Trost <nico.trost@amd.com>
2018-05-09 12:53:24 +0200

    csr2coo: index_base_one support

--- library/src/conversion/csr2coo_device.h
+++ library/src/conversion/csr2coo_device.h
@@ -12,7 +12,8 @@ template <rocsparse_int THREADS>
 __global__
 void csr2coo_kernel(rocsparse_int m,
                     const rocsparse_int *csr_row_ptr,
-                    rocsparse_int *coo_row_ind)
+                    rocsparse_int *coo_row_ind,
+                    rocsparse_index_base idx_base)
 {
     rocsparse_int gid  = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
     rocsparse_int lid  = hipThreadIdx_x % THREADS;
@@ -23,7 +24,7 @@ void csr2coo_kernel(rocsparse_int m,
     {
         for(rocsparse_int aj=csr_row_ptr[ai]+lid; aj<csr_row_ptr[ai+1]; aj+=THREADS)
         {
-            coo_row_ind[aj] = ai;
+            coo_row_ind[aj-idx_base] = ai + idx_base;
         }
     }
 }
