103870a4356b856c93337db01bab66892c9be918
Nico Trost <nico.trost@amd.com>
2018-05-14 08:39:41 +0200

    testing csrmv: now using sparse matrix generator
    [ROCm/rocSPARSEcommit: e6b02c26c07d8737fa7c15bd635a69881ce32ec5]

--- clients/tests/test_csrmv.cpp
+++ clients/tests/test_csrmv.cpp
@@ -9,12 +9,14 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-typedef std::tuple<int, int, double, double> csrmv_tuple;
+typedef rocsparse_index_base base;
+typedef std::tuple<int, int, double, double, base> csrmv_tuple;
 
 int csr_M_range[] = {-1, 0, 10, 500, 7111, 10000};
 int csr_N_range[] = {-3, 0, 33, 842, 4441, 10000};
 std::vector<double> csr_alpha_range = {2.0, 3.0};
 std::vector<double> csr_beta_range = {0.0, 1.0};
+base csr_idxbase_range[] = {rocsparse_index_base_zero};
 
 class parameterized_csrmv : public testing::TestWithParam<csrmv_tuple>
 {
@@ -28,30 +30,31 @@ class parameterized_csrmv : public testing::TestWithParam<csrmv_tuple>
 Arguments setup_csrmv_arguments(csrmv_tuple tup)
 {
     Arguments arg;
-    arg.M       = std::get<0>(tup);
-    arg.N       = std::get<1>(tup);
-    arg.alpha   = std::get<2>(tup);
-    arg.beta    = std::get<3>(tup);
-    arg.timing  = 0;
+    arg.M        = std::get<0>(tup);
+    arg.N        = std::get<1>(tup);
+    arg.alpha    = std::get<2>(tup);
+    arg.beta     = std::get<3>(tup);
+    arg.idx_base = std::get<4>(tup);
+    arg.timing   = 0;
     return arg;
 }
 
 TEST(csrmv_bad_arg, csrmv_float)
 {
-    testing_csrmv_bad_arg<rocsparse_int, float>();
+    testing_csrmv_bad_arg<float>();
 }
 
 TEST_P(parameterized_csrmv, csrmv_float)
 {
     Arguments arg = setup_csrmv_arguments(GetParam());
-    rocsparse_status status = testing_csrmv<rocsparse_int, float>(arg);
+    rocsparse_status status = testing_csrmv<float>(arg);
     EXPECT_EQ(status, rocsparse_status_success);
 }
 
 TEST_P(parameterized_csrmv, csrmv_double)
 {
     Arguments arg = setup_csrmv_arguments(GetParam());
-    rocsparse_status status = testing_csrmv<rocsparse_int, double>(arg);
+    rocsparse_status status = testing_csrmv<double>(arg);
     EXPECT_EQ(status, rocsparse_status_success);
 }
 
@@ -59,4 +62,5 @@ INSTANTIATE_TEST_CASE_P(csrmv, parameterized_csrmv,
                         testing::Combine(testing::ValuesIn(csr_M_range),
                                          testing::ValuesIn(csr_N_range),
                                          testing::ValuesIn(csr_alpha_range),
-                                         testing::ValuesIn(csr_beta_range)));
+                                         testing::ValuesIn(csr_beta_range),
+                                         testing::ValuesIn(csr_idxbase_range)));
