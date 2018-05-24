22c1a88ae3255a22dc54cd00b3c2015b9d89987d
Nico Trost <nico.trost@amd.com>
2018-05-24 11:58:33 +0200

    coomv: tests, benchmark and example

---
+++ clients/samples/example_coomv.cpp
@@ -0,0 +1,172 @@
+/* ************************************************************************
+ * Copyright 2018 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#include "utility.hpp"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <rocsparse.h>
+
+int main(int argc, char* argv[])
+{
+    // Parse command line
+    if(argc < 2)
+    {
+        fprintf(stderr, "%s <ndim> [<trials> <batch_size>]\n", argv[0]);
+        return -1;
+    }
+
+    int ndim       = atoi(argv[1]);
+    int trials     = 200;
+    int batch_size = 1;
+
+    if(argc > 2)
+    {
+        trials = atoi(argv[2]);
+    }
+    if(argc > 3)
+    {
+        batch_size = atoi(argv[3]);
+    }
+
+    // rocSPARSE handle
+    rocsparse_handle handle;
+    rocsparse_create_handle(&handle);
+
+    hipDeviceProp_t devProp;
+    int device_id = 0;
+
+    hipGetDevice(&device_id);
+    hipGetDeviceProperties(&devProp, device_id);
+    printf("Device: %s\n", devProp.name);
+
+    // Generate problem
+    std::vector<int> hAptr;
+    std::vector<int> hAcol;
+    std::vector<double> hAval;
+    int m   = gen_2d_laplacian(ndim, hAptr, hAcol, hAval, rocsparse_index_base_zero);
+    int n   = m;
+    int nnz = hAptr[m];
+
+    // Convert to COO matrix
+    std::vector<int> hArow(nnz);
+
+    for(int i = 0; i < m; ++i)
+    {
+        for(int j = hAptr[i]; j < hAptr[i + 1]; ++j)
+        {
+            hArow[j] = i;
+        }
+    }
+
+    // Sample some random data
+    srand(12345ULL);
+
+    double halpha = static_cast<double>(rand()) / RAND_MAX;
+    double hbeta  = 0.0;
+
+    std::vector<double> hx(m);
+    rocsparse_init(hx, 1, m);
+
+    // Matrix descriptor
+    rocsparse_mat_descr descrA;
+    rocsparse_create_mat_descr(&descrA);
+
+    // Offload data to device
+    int* dArow    = NULL;
+    int* dAcol    = NULL;
+    double* dAval = NULL;
+    double* dx    = NULL;
+    double* dy    = NULL;
+
+    hipMalloc((void**)&dArow, sizeof(int) * nnz);
+    hipMalloc((void**)&dAcol, sizeof(int) * nnz);
+    hipMalloc((void**)&dAval, sizeof(double) * nnz);
+    hipMalloc((void**)&dx, sizeof(double) * m);
+    hipMalloc((void**)&dy, sizeof(double) * m);
+
+    hipMemcpy(dArow, hArow.data(), sizeof(int) * nnz, hipMemcpyHostToDevice);
+    hipMemcpy(dAcol, hAcol.data(), sizeof(int) * nnz, hipMemcpyHostToDevice);
+    hipMemcpy(dAval, hAval.data(), sizeof(double) * nnz, hipMemcpyHostToDevice);
+    hipMemcpy(dx, hx.data(), sizeof(double) * m, hipMemcpyHostToDevice);
+
+    // Warm up
+    for(int i = 0; i < 10; ++i)
+    {
+        // Call rocsparse coomv
+        rocsparse_dcoomv(handle,
+                         rocsparse_operation_none,
+                         m,
+                         n,
+                         nnz,
+                         &halpha,
+                         descrA,
+                         dAval,
+                         dArow,
+                         dAcol,
+                         dx,
+                         &hbeta,
+                         dy);
+    }
+
+    // Device synchronization
+    hipDeviceSynchronize();
+
+    // Start time measurement
+    double time = get_time_us();
+
+    // COO matrix vector multiplication
+    for(int i = 0; i < trials; ++i)
+    {
+        for(int i = 0; i < batch_size; ++i)
+        {
+            // Call rocsparse coomv
+            rocsparse_dcoomv(handle,
+                             rocsparse_operation_none,
+                             m,
+                             n,
+                             nnz,
+                             &halpha,
+                             descrA,
+                             dAval,
+                             dArow,
+                             dAcol,
+                             dx,
+                             &hbeta,
+                             dy);
+        }
+
+        // Device synchronization
+        hipDeviceSynchronize();
+    }
+
+    time = (get_time_us() - time) / (trials * batch_size * 1e3);
+    double bandwidth =
+        static_cast<double>(sizeof(double) * (4 * m + nnz) + sizeof(rocsparse_int) * (2 * nnz)) /
+        time / 1e6;
+    double gflops = static_cast<double>(3 * nnz) / time / 1e6;
+    printf("m\t\tn\t\tnnz\t\talpha\tbeta\tGFlops\tGB/s\tusec\n");
+    printf("%8d\t%8d\t%9d\t%0.2lf\t%0.2lf\t%0.2lf\t%0.2lf\t%0.2lf\n",
+           m,
+           n,
+           nnz,
+           halpha,
+           hbeta,
+           gflops,
+           bandwidth,
+           time);
+
+    // Clear up on device
+    hipFree(dArow);
+    hipFree(dAcol);
+    hipFree(dAval);
+    hipFree(dx);
+    hipFree(dy);
+
+    rocsparse_destroy_mat_descr(descrA);
+    rocsparse_destroy_handle(handle);
+
+    return 0;
+}
---
+++ clients/tests/test_coomv.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************
+ * Copyright 2018 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#include "testing_coomv.hpp"
+#include "utility.hpp"
+
+#include <rocsparse.h>
+#include <gtest/gtest.h>
+#include <vector>
+
+typedef rocsparse_index_base base;
+typedef std::tuple<int, int, double, double, base> coomv_tuple;
+
+int coo_M_range[] = {-1, 0, 10, 500, 7111, 10000};
+int coo_N_range[] = {-3, 0, 33, 842, 4441, 10000};
+
+std::vector<double> coo_alpha_range = {2.0, 3.0};
+std::vector<double> coo_beta_range  = {0.0, 0.67, 1.0};
+
+base coo_idxbase_range[] = {rocsparse_index_base_zero};
+
+class parameterized_coomv : public testing::TestWithParam<coomv_tuple>
+{
+    protected:
+    parameterized_coomv() {}
+    virtual ~parameterized_coomv() {}
+    virtual void SetUp() {}
+    virtual void TearDown() {}
+};
+
+Arguments setup_coomv_arguments(coomv_tuple tup)
+{
+    Arguments arg;
+    arg.M        = std::get<0>(tup);
+    arg.N        = std::get<1>(tup);
+    arg.alpha    = std::get<2>(tup);
+    arg.beta     = std::get<3>(tup);
+    arg.idx_base = std::get<4>(tup);
+    arg.timing   = 0;
+    return arg;
+}
+
+TEST(coomv_bad_arg, coomv_float) { testing_coomv_bad_arg<float>(); }
+
+TEST_P(parameterized_coomv, coomv_float)
+{
+    Arguments arg = setup_coomv_arguments(GetParam());
+
+    rocsparse_status status = testing_coomv<float>(arg);
+    EXPECT_EQ(status, rocsparse_status_success);
+}
+
+TEST_P(parameterized_coomv, coomv_double)
+{
+    Arguments arg = setup_coomv_arguments(GetParam());
+
+    rocsparse_status status = testing_coomv<double>(arg);
+    EXPECT_EQ(status, rocsparse_status_success);
+}
+
+INSTANTIATE_TEST_CASE_P(coomv,
+                        parameterized_coomv,
+                        testing::Combine(testing::ValuesIn(coo_M_range),
+                                         testing::ValuesIn(coo_N_range),
+                                         testing::ValuesIn(coo_alpha_range),
+                                         testing::ValuesIn(coo_beta_range),
+                                         testing::ValuesIn(coo_idxbase_range)));
