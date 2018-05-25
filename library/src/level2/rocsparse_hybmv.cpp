4f9aee25c7b1d6cb4aaa203b3f77956df2678e8d
Nico Trost <nico.trost@amd.com>
2018-05-25 18:00:19 +0200

    added hyb_partition_auto and hyb_partition_user for csr2hyb and hybmv - still experimental
    [ROCm/rocSPARSEcommit: 9d7679a249f0b947f513dfd5ec967407098561b5]

--- library/src/level2/rocsparse_hybmv.cpp
+++ library/src/level2/rocsparse_hybmv.cpp
@@ -3,8 +3,10 @@
  * ************************************************************************ */
 
 #include "rocsparse.h"
+#include "definitions.h"
 #include "handle.h"
 #include "utility.h"
+#include "coomv_device.h"
 #include "ellmv_device.h"
 
 #include <hip/hip_runtime.h>
@@ -37,6 +39,32 @@ __global__ void ellmvn_kernel_device_pointer(rocsparse_int m,
                                              rocsparse_index_base idx_base)
 {
     ellmvn_device(m, n, ell_width, *alpha, ell_col_ind, ell_val, x, *beta, y, idx_base);
+}
+
+template <typename T, rocsparse_int BLOCKSIZE, rocsparse_int WARPSIZE>
+__global__ void coomvn_warp_host_pointer(rocsparse_int nnz,
+                                         rocsparse_int loops,
+                                         T alpha,
+                                         const rocsparse_int* coo_row_ind,
+                                         const rocsparse_int* coo_col_ind,
+                                         const T* coo_val,
+                                         const T* x,
+                                         T* y,
+                                         rocsparse_int* row_block_red,
+                                         T* val_block_red,
+                                         rocsparse_index_base idx_base)
+{
+    coomvn_general_warp_reduce<T, BLOCKSIZE, WARPSIZE>(nnz,
+                                                       loops,
+                                                       alpha,
+                                                       coo_row_ind,
+                                                       coo_col_ind,
+                                                       coo_val,
+                                                       x,
+                                                       y,
+                                                       row_block_red,
+                                                       val_block_red,
+                                                       idx_base);
 }
 
 template <typename T>
@@ -100,10 +128,11 @@ rocsparse_status rocsparse_hybmv_template(rocsparse_handle handle,
         // TODO
         return rocsparse_status_not_implemented;
     }
-    if(hyb->partition != rocsparse_hyb_partition_max)
-    {
-        return rocsparse_status_not_implemented;
-    }
+// TODO check partition type
+//    if(hyb->partition != rocsparse_hyb_partition_max)
+//    {
+//        return rocsparse_status_not_implemented;
+//    }
 
     // Check sizes
     if(hyb->m < 0)
@@ -215,6 +244,69 @@ rocsparse_status rocsparse_hybmv_template(rocsparse_handle handle,
                                    *beta,
                                    y,
                                    descr->base);
+            }
+
+            // COO part
+            if(hyb->coo_nnz > 0)
+            {
+// TODO
+#define COOMVN_DIM 128
+        rocsparse_int maxthreads = handle->properties.maxThreadsPerBlock;
+        rocsparse_int nprocs     = handle->properties.multiProcessorCount;
+        rocsparse_int maxblocks  = (nprocs * maxthreads - 1) / COOMVN_DIM + 1;
+        rocsparse_int minblocks  = (hyb->coo_nnz - 1) / COOMVN_DIM + 1;
+
+        rocsparse_int nblocks = maxblocks < minblocks ? maxblocks : minblocks;
+        rocsparse_int nwarps  = nblocks * (COOMVN_DIM / handle->warp_size);
+        rocsparse_int nloops  = (hyb->coo_nnz / handle->warp_size + 1) / nwarps + 1;
+
+        dim3 coomvn_blocks(nblocks);
+        dim3 coomvn_threads(COOMVN_DIM);
+
+        rocsparse_int* row_block_red = NULL;
+        T* val_block_red             = NULL;
+
+        RETURN_IF_HIP_ERROR(hipMalloc((void**)&row_block_red, sizeof(rocsparse_int) * nwarps));
+        RETURN_IF_HIP_ERROR(hipMalloc((void**)&val_block_red, sizeof(T) * nwarps));
+
+                hipLaunchKernelGGL((coomvn_warp_host_pointer<T, COOMVN_DIM, 64>),
+                                   coomvn_blocks,
+                                   coomvn_threads,
+                                   0,
+                                   stream,
+                                   hyb->coo_nnz,
+                                   nloops,
+                                   *alpha,
+                                   hyb->coo_row_ind,
+                                   hyb->coo_col_ind,
+                                   hyb->coo_val,
+                                   x,
+                                   y,
+                                   row_block_red,
+                                   val_block_red,
+                                   descr->base);
+
+        hipLaunchKernelGGL((coomvn_general_block_reduce<T, COOMVN_DIM>),
+                           dim3(1),
+                           coomvn_threads,
+                           0,
+                           stream,
+                           nwarps,
+                           row_block_red,
+                           val_block_red,
+                           y);
+
+        RETURN_IF_HIP_ERROR(hipFree(row_block_red));
+        RETURN_IF_HIP_ERROR(hipFree(val_block_red));
+#undef COOMVN_DIM
+
+
+
+
+
+
+
+
             }
         }
 #undef ELLMVN_DIM
