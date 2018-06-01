931c94f3ce0ce237d3de54a0e5cdf8977f17e8df
Nico Trost <nico.trost@amd.com>
2018-06-01 10:52:39 +0200

    added sparse level1 placeholders
    [ROCm/rocSPARSEcommit: b78b0a53b18ee29628fce091a2ac14620e370825]

---
+++ library/src/level1/rocsparse_dotci.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************
+ * Copyright 2018 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#pragma once
+#ifndef ROCSPARSE_DOTCI_HPP
+#define ROCSPARSE_DOTCI_HPP
+
+#include "rocsparse.h"
+#include "handle.h"
+#include "utility.h"
+#include "dotci_device.h"
+
+#include <hip/hip_runtime.h>
+
+template <typename T>
+rocsparse_status rocsparse_dotci_template(rocsparse_handle handle,
+                                          rocsparse_int nnz,
+                                          const T* x_val,
+                                          const rocsparse_int* x_ind,
+                                          const T* y,
+                                          T* result,
+                                          rocsparse_index_base idx_base)
+{
+    return rocsparse_status_not_implemented;
+}
+
+#endif // ROCSPARSE_DOTCI_HPP
