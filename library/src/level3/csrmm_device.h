ef9c334ec5e7de7e9ab68a70c20080702fa56fbd
Nico Trost <nico.trost@amd.com>
2018-06-15 22:46:38 +0200

    csrmm prep #2
    [ROCm/rocSPARSEcommit: 82ed7d85a365666a9e7d0cb508d3ac098ab59aa8]

--- library/src/level3/csrmm_device.h
+++ library/src/level3/csrmm_device.h
@@ -5,7 +5,20 @@
 #include <hip/hip_runtime.h>
 
 template <typename T>
-static __device__ void csrmmn_general_device()
+static __device__ void csrmmn_general_device(rocsparse_int m,
+                                             rocsparse_int n,
+                                             rocsparse_int k,
+                                             rocsparse_int nnz,
+                                             T alpha,
+                                             const rocsparse_int* csr_row_ptr,
+                                             const rocsparse_int* csr_col_ind,
+                                             const T* csr_val,
+                                             const T* B,
+                                             rocsparse_int ldb,
+                                             T beta,
+                                             T* C,
+                                             rocsparse_int ldc,
+                                             rocsparse_index_base idx_base)
 {
 }
 
