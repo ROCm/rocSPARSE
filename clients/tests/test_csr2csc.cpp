2f9888761146e3a800e78b684be7dbaa209b24b9
Nico Trost <nico.trost@amd.com>
2018-07-29 12:28:08 +0200

    csr2csc test with real matrices
    [ROCm/rocSPARSEcommit: 1754055275ebcca6e2e77576fca3c86c5816f1dc]

--- clients/tests/test_csr2csc.cpp
+++ clients/tests/test_csr2csc.cpp
@@ -9,15 +9,30 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-typedef std::tuple<int, int, rocsparse_action, rocsparse_index_base> csr2csc_tuple;
+typedef std::tuple<int, int, rocsparse_action, rocsparse_index_base, std::string> csr2csc_tuple;
 
-int csr2csc_M_range[] = {-1, 0, 10, 500, 872, 1000};
-int csr2csc_N_range[] = {-3, 0, 33, 242, 623, 1000};
+int csr2csc_M_range[] = {-99, -1, 0, 10, 500, 872, 1000};
+int csr2csc_N_range[] = {-99, 0, 33, 242, 623, 1000};
 
 rocsparse_action csr2csc_action_range[] = {rocsparse_action_numeric, rocsparse_action_symbolic};
 
 rocsparse_index_base csr2csc_csr_base_range[] = {rocsparse_index_base_zero,
                                                  rocsparse_index_base_one};
+
+std::string csr2csc_bin[] = {"rma10.bin",
+                             "mac_econ_fwd500.bin",
+                             "bibd_22_8.bin",
+                             "mc2depi.bin",
+                             "scircuit.bin",
+                             "ASIC_320k.bin",
+                             "bmwcra_1.bin",
+                             "nos1.bin",
+                             "nos2.bin",
+                             "nos3.bin",
+                             "nos4.bin",
+                             "nos5.bin",
+                             "nos6.bin",
+                             "nos7.bin"};
 
 class parameterized_csr2csc : public testing::TestWithParam<csr2csc_tuple>
 {
@@ -36,6 +51,25 @@ Arguments setup_csr2csc_arguments(csr2csc_tuple tup)
     arg.action   = std::get<2>(tup);
     arg.idx_base = std::get<3>(tup);
     arg.timing   = 0;
+
+    // Determine absolute path of test matrix
+    std::string bin_file = std::get<4>(tup);
+
+    // Get current executables absolute path
+    char path_exe[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", path_exe, sizeof(path_exe) - 1);
+    if(len < 14)
+    {
+        path_exe[0] = '\0';
+    }
+    else
+    {
+        path_exe[len - 14] = '\0';
+    }
+
+    // Matrices are stored at the same path in matrices directory
+    arg.filename = std::string(path_exe) + "matrices/" + bin_file;
+
     return arg;
 }
 
@@ -62,4 +96,5 @@ INSTANTIATE_TEST_CASE_P(csr2csc,
                         testing::Combine(testing::ValuesIn(csr2csc_M_range),
                                          testing::ValuesIn(csr2csc_N_range),
                                          testing::ValuesIn(csr2csc_action_range),
-                                         testing::ValuesIn(csr2csc_csr_base_range)));
+                                         testing::ValuesIn(csr2csc_csr_base_range),
+                                         testing::ValuesIn(csr2csc_bin)));
