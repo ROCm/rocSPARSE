b60e50743d999f4aa1f9f56b2235559463d8dd0a
Nico Trost <nico.trost@amd.com>
2018-08-30 07:01:37 +0000

    actually took the wrong version of rocPRIM - should work now

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -47,7 +47,7 @@ if(HIP_PLATFORM STREQUAL "hcc")
     message(STATUS "Downloading rocPRIM.")
     download_project(PROJ    rocPRIM
          GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
-         GIT_TAG             0702a4a797f021bf7098d6b8cca046c85f4cb22d # TODO change back to master once rocm docker image is updated
+         GIT_TAG             caef132d64b29a7d857eb68af5323fc302d26766 # TODO change back to master once rocm docker image is updated
          INSTALL_DIR         ${ROCPRIM_ROOT}
          CMAKE_ARGS          -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_CXX_COMPILER=/opt/rocm/bin/hcc
          LOG_DOWNLOAD        TRUE
