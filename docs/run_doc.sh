545870ad812ed7909017e848a157332774e9a39e
Nico Trost <nico.trost@amd.com>
2018-09-06 09:54:41 +0200

    initial documentation

---
+++ docs/run_doc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -d docBin ]; then
+    rm -rf docBin
+fi
+
+sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-functions.h > rocsparse-functions.h
+sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-auxiliary.h > rocsparse-auxiliary.h
+cp ../library/include/rocsparse-types.h rocsparse-types.h
+doxygen Doxyfile
+
+cd source
+make clean
+make html
+cd ..
+
+rm rocsparse-functions.h
+rm rocsparse-auxiliary.h
+rm rocsparse-types.h
