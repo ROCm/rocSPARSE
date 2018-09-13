bdba9094ecff6f088ae0b14c470fec4ce97bb96a
Nico Trost <nico.trost@amd.com>
2018-09-13 16:28:54 +0200

    more docs

--- library/include/rocsparse-types.h
+++ library/include/rocsparse-types.h
@@ -24,17 +24,46 @@ typedef int32_t rocsparse_int;
 
 /*! \ingroup types_module
  *  \brief Handle to the rocSPARSE library context queue.
+ *
+ *  \details
+ *  The rocSPARSE handle is a structure holding the rocSPARSE library context. It must
+ *  be initialized using rocsparse_create_handle() and the returned handle must be
+ *  passed to all subsequent library function calls. It should be destroyed at the end
+ *  using rocsparse_destroy_handle().
  */
 typedef struct _rocsparse_handle* rocsparse_handle;
+
 /*! \ingroup types_module
  *  \brief Descriptor of the matrix.
+ *
+ *  \details
+ *  The rocSPARSE matrix descriptor is a structure holding all properties of a matrix.
+ *  It must be initialized using rocsparse_create_mat_descr() and the returned descriptor
+ *  must be passed to all subsequent library calls that involve the matrix. It should be
+ *  destroyed at the end using rocsparse_destroy_mat_descr().
+ */
 typedef struct _rocsparse_mat_descr* rocsparse_mat_descr;
+
 /*! \ingroup types_module
  *  \brief HYB matrix storage format.
+ *
+ *  \details
+ *  The rocSPARSE HYB matrix structure holds the HYB matrix. It must be initialized using
+ *  rocsparse_create_hyb_mat() and the returned HYB matrix must be passed to all
+ *  subsequent library calls that involve the matrix. It should be destroyed at the end
+ *  using rocsparse_destroy_hyb_mat(). TODO For more details on the HYB format, see HYB storage format.
  */
 typedef struct _rocsparse_hyb_mat* rocsparse_hyb_mat;
+
 /*! \ingroup types_module
  *  \brief Info structure to hold all matrix meta data.
+ *
+ *  \details
+ *  The rocSPARSE matrix info is a structure holding all matrix information that is
+ *  gathered during analysis routines. It must be initialized using
+ *  rocsparse_create_mat_info() and the returned info structure must be passed to all
+ *  subsequent library calls that require additional matrix information. It should be
+ *  destroyed at the end using rocsparse_destroy_mat_info().
  */
 typedef struct _rocsparse_mat_info* rocsparse_mat_info;
 
@@ -44,6 +73,9 @@ extern "C" {
 
 /*! \ingroup types_module
  *  \brief Specify whether the matrix is to be transposed or not.
+ *
+ *  \details
+ *  The \ref rocsparse_operation indicates the operation performed with the given matrix.
  */
 typedef enum rocsparse_operation_ {
     rocsparse_operation_none                = 111, /**< Operate with matrix. */
@@ -53,6 +85,12 @@ typedef enum rocsparse_operation_ {
 
 /*! \ingroup types_module
  *  \brief Specify the matrix index base.
+ *
+ *  \details
+ *  The \ref rocsparse_index_base indicates the index base of the indices. For a
+ *  given \ref rocsparse_mat_descr, the \ref rocsparse_index_base can be set using
+ *  rocsparse_set_mat_index_base(). The current \ref rocsparse_index_base of a matrix
+ *  can be obtained by rocsparse_get_mat_index_base().
  */
 typedef enum rocsparse_index_base_ {
     rocsparse_index_base_zero = 0, /**< zero based indexing. */
@@ -61,6 +99,12 @@ typedef enum rocsparse_index_base_ {
 
 /*! \ingroup types_module
  *  \brief Specify the matrix type.
+ *
+ *  \details
+ *  The \ref rocsparse_matrix_type indices the type of a matrix. For a given
+ *  \ref rocsparse_mat_descr, the \ref rocsparse_matrix_type can be set using
+ *  rocsparse_set_mat_type(). The current \ref rocsparse_matrix_type of a matrix can be
+ *  obtained by rocsparse_get_mat_type().
  */
 typedef enum rocsparse_matrix_type_ {
     rocsparse_matrix_type_general    = 0, /**< general matrix type. */
@@ -71,6 +115,14 @@ typedef enum rocsparse_matrix_type_ {
 
 /*! \ingroup types_module
  *  \brief Indicates if the diagonal entries are unity.
+ *
+ *  \details
+ *  The \ref rocsparse_diag_type indicates whether the diagonal entries of a matrix are
+ *  unity or not. If \ref rocsparse_diag_type_unit is specified, all present diagonal
+ *  values will be ignored. For a given \ref rocsparse_mat_descr, the
+ *  \ref rocsparse_diag_type can be set using rocsparse_set_mat_diag_type(). The current
+ *  \ref rocsparse_diag_type of a matrix can be obtained by
+ *  rocsparse_get_mat_diag_type().
  */
 typedef enum rocsparse_diag_type_ {
     rocsparse_diag_type_non_unit = 0, /**< diagonal entries are non-unity. */
@@ -79,6 +131,13 @@ typedef enum rocsparse_diag_type_ {
 
 /*! \ingroup types_module
  *  \brief Specify the matrix fill mode.
+ *
+ *  \details
+ *  The \ref rocsparse_fill_mode indicates whether the lower or the upper part is stored
+ *  in a sparse triangular matrix. For a given \ref rocsparse_mat_descr, the
+ *  \ref rocsparse_fill_mode can be set using rocsparse_set_mat_fill_mode(). The current
+ *  \ref rocsparse_fill_mode of a matrix can be obtained by
+ *  rocsparse_get_mat_fill_mode().
  */
 typedef enum rocsparse_fill_mode_ {
     rocsparse_fill_mode_lower = 0, /**< lower triangular part is stored. */
@@ -87,6 +146,10 @@ typedef enum rocsparse_fill_mode_ {
 
 /*! \ingroup types_module
  *  \brief Specify where the operation is performed on.
+ *
+ *  \details
+ *  The \ref rocsparse_action indicates whether the operation is performed on the full
+ *  matrix, or only on the sparsity pattern of the matrix.
  */
 typedef enum rocsparse_action_ {
     rocsparse_action_symbolic = 0, /**< Operate only on indices. */
@@ -95,6 +158,10 @@ typedef enum rocsparse_action_ {
 
 /*! \ingroup types_module
  *  \brief HYB matrix partitioning type.
+ *
+ *  \details
+ *  The \ref rocsparse_hyb_partition type indicates how the hybrid format partitioning
+ *  between COO and ELL storage formats is performed.
  */
 typedef enum rocsparse_hyb_partition_ {
     rocsparse_hyb_partition_auto = 0, /**< automatically decide on ELL nnz per row. */
@@ -104,6 +171,12 @@ typedef enum rocsparse_hyb_partition_ {
 
 /*! \ingroup types_module
  *  \brief Specify policy in analysis functions.
+ *
+ *  \details
+ *  The \ref rocsparse_analysis_policy specifies whether gathered analysis data should be
+ *  re-used or not. If meta data from a previous e.g. rocsparse_csrilu0_analysis() call
+ *  is available, it can be re-used for subsequent calls to e.g.
+ *  rocsparse_csrsv_analysis() and greatly improve performance of the analysis function.
  */
 typedef enum rocsparse_analysis_policy_ {
     rocsparse_analysis_policy_reuse = 0, /**< try to re-use meta data. */
@@ -112,6 +185,9 @@ typedef enum rocsparse_analysis_policy_ {
 
 /*! \ingroup types_module
  *  \brief Specify policy in triangular solvers and factorizations.
+ *
+ *  \details
+ *  This is a placeholder.
  */
 typedef enum rocsparse_solve_policy_ {
     rocsparse_solve_policy_auto = 0 /**< automatically decide on level information. */
@@ -119,6 +195,12 @@ typedef enum rocsparse_solve_policy_ {
 
 /*! \ingroup types_module
  *  \brief Indicates if the pointer is device pointer or host pointer.
+ *
+ *  \details
+ *  The \ref rocsparse_pointer_mode indicates whether scalar values are passed by
+ *  reference on the host or device. The \ref rocsparse_pointer_mode can be changed by
+ *  rocsparse_set_pointer_mode(). The currently used pointer mode can be obtained by
+ *  rocsparse_get_pointer_mode().
  */
 typedef enum rocsparse_pointer_mode_ {
     rocsparse_pointer_mode_host   = 0, /**< scalar pointers are in host memory. */
@@ -127,6 +209,10 @@ typedef enum rocsparse_pointer_mode_ {
 
 /*! \ingroup types_module
  *  \brief Indicates if layer is active with bitmask.
+ *
+ *  \details
+ *  The \ref rocsparse_layer_mode bit mask indicates the logging characteristics. See
+ *  TODO rocsparse_logging for more informations.
  */
 typedef enum rocsparse_layer_mode {
     rocsparse_layer_mode_none      = 0b0000000000, /**< layer is not active. */
@@ -135,7 +221,11 @@ typedef enum rocsparse_layer_mode {
 } rocsparse_layer_mode;
 
 /*! \ingroup types_module
- *  \brief rocsparse status codes definition.
+ *  \brief List of rocsparse status codes definition.
+ *
+ *  \details
+ *  This is a list of the \ref rocsparse_status types that are used by the rocSPARSE
+ *  library.
  */
 typedef enum rocsparse_status_ {
     rocsparse_status_success         = 0, /**< success. */
