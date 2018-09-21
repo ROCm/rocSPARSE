4a167029e5254b690ec912d03b03ac4cf73a9c87
Nico Trost <nico.trost@amd.com>
2018-09-21 10:29:05 +0200

    added libnuma1 to dependencies
    [ROCm/rocSPARSEcommit: 08577b784fd691934cdf95827b6a7adb3c4312f3]

--- install.sh
+++ install.sh
@@ -114,9 +114,9 @@ install_packages( )
   fi
 
   # dependencies needed for library and clients to build
-  local library_dependencies_ubuntu=( "make" "cmake-curses-gui" "hip_hcc" "pkg-config" )
-  local library_dependencies_centos=( "epel-release" "make" "cmake3" "hip_hcc" "gcc-c++" )
-  local library_dependencies_fedora=( "make" "cmake" "hip_hcc" "gcc-c++" "libcxx-devel" "rpm-build" )
+  local library_dependencies_ubuntu=( "make" "cmake-curses-gui" "hip_hcc" "pkg-config" "libnuma1" )
+  local library_dependencies_centos=( "epel-release" "make" "cmake3" "hip_hcc" "gcc-c++" "numactl-libs" )
+  local library_dependencies_fedora=( "make" "cmake" "hip_hcc" "gcc-c++" "libcxx-devel" "rpm-build" "numactl-libs" )
 
   if [[ "${build_cuda}" == true ]]; then
     # Ideally, this could be cuda-cusparse-dev, but the package name has a version number in it
