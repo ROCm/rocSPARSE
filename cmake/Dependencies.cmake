60475982caaa6c28c7b5eeda75a448fd152197e7
Nico Trost <nico.trost@amd.com>
2018-10-19 09:01:40 +0200

    testing change in cmake files to overcome HIP_PLATFORM not set issue

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -30,15 +30,7 @@ find_package(Git REQUIRED)
 include(cmake/DownloadProject/DownloadProject.cmake)
 
 # HIP configuration
-if(HIP_PLATFORM STREQUAL "hcc")
-  # Workaround until hcc & hip cmake modules fixes symlink logic in their config files.
-  # (Thanks to rocBLAS devs for finding workaround for this problem!)
-  list(APPEND CMAKE_PREFIX_PATH /opt/rocm/hcc /opt/rocm/hip)
-  # Ignore hcc warning: argument unused during compilation: '-isystem /opt/rocm/hip/include'
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unused-command-line-argument")
-  find_package(hcc REQUIRED CONFIG PATHS /opt/rocm)
-  find_package(HIP REQUIRED CONFIG PATHS /opt/rocm)
-elseif(HIP_PLATFORM STREQUAL "nvcc")
+if(HIP_PLATFORM STREQUAL "nvcc")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Xcompiler -Wall")
   set(CMAKE_C_COMPILE_OPTIONS_PIC "-Xcompiler ${CMAKE_C_COMPILE_OPTIONS_PIC}" )
   set(CMAKE_CXX_COMPILE_OPTIONS_PIC "-Xcompiler ${CMAKE_CXX_COMPILE_OPTIONS_PIC}" )
@@ -55,10 +47,36 @@ elseif(HIP_PLATFORM STREQUAL "nvcc")
   set(CMAKE_C_COMPILE_OPTIONS_VISIBILITY_INLINES_HIDDEN "-Xcompiler ${CMAKE_C_COMPILE_OPTIONS_VISIBILITY_INLINES_HIDDEN}" )
   set(CMAKE_CXX_COMPILE_OPTIONS_VISIBILITY_INLINES_HIDDEN "-Xcompiler ${CMAKE_CXX_COMPILE_OPTIONS_VISIBILITY_INLINES_HIDDEN}" )
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -arch sm_35")
+else()
+  # Workaround until hcc & hip cmake modules fixes symlink logic in their config files.
+  # (Thanks to rocBLAS devs for finding workaround for this problem!)
+  list(APPEND CMAKE_PREFIX_PATH /opt/rocm/hcc /opt/rocm/hip)
+  # Ignore hcc warning: argument unused during compilation: '-isystem /opt/rocm/hip/include'
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unused-command-line-argument")
+  find_package(hcc REQUIRED CONFIG PATHS /opt/rocm)
+  find_package(HIP REQUIRED CONFIG PATHS /opt/rocm)
 endif()
 
 # rocPRIM package
-if(HIP_PLATFORM STREQUAL "hcc")
+if(HIP_PLATFORM STREQUAL "nvcc")
+  find_package(HIPCUB QUIET CONFIG PATHS /opt/rocm)
+  if(NOT HIPCUB_FOUND)
+    set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
+    message(STATUS "Downloading rocPRIM.")
+    download_project(PROJ    rocPRIM
+         GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
+         GIT_TAG             master
+         INSTALL_DIR         ${ROCPRIM_ROOT}
+         CMAKE_ARGS          -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_CXX_COMPILER=${HIP_HIPCC_EXECUTABLE}
+         LOG_DOWNLOAD        TRUE
+         LOG_CONFIGURE       TRUE
+         LOG_INSTALL         TRUE
+         BUILD_PROJECT       TRUE
+         UPDATE_DISCONNECT   TRUE
+    )
+  find_package(HIPCUB REQUIRED CONFIG PATHS ${ROCPRIM_ROOT})
+  endif()
+else()
   find_package(ROCPRIM QUIET CONFIG PATHS /opt/rocm)
   find_package(HIPCUB QUIET CONFIG PATHS /opt/rocm)
   if(NOT ROCPRIM_FOUND)
@@ -76,24 +94,6 @@ if(HIP_PLATFORM STREQUAL "hcc")
          UPDATE_DISCONNECT   TRUE
     )
   find_package(ROCPRIM REQUIRED CONFIG PATHS ${ROCPRIM_ROOT})
-  find_package(HIPCUB REQUIRED CONFIG PATHS ${ROCPRIM_ROOT})
-  endif()
-elseif(HIP_PLATFORM STREQUAL "nvcc")
-  find_package(HIPCUB QUIET CONFIG PATHS /opt/rocm)
-  if(NOT HIPCUB_FOUND)
-    set(ROCPRIM_ROOT ${CMAKE_CURRENT_BINARY_DIR}/rocPRIM CACHE PATH "")
-    message(STATUS "Downloading rocPRIM.")
-    download_project(PROJ    rocPRIM
-         GIT_REPOSITORY      https://github.com/ROCmSoftwarePlatform/rocPRIM.git
-         GIT_TAG             master
-         INSTALL_DIR         ${ROCPRIM_ROOT}
-         CMAKE_ARGS          -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_TEST=OFF -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DCMAKE_CXX_COMPILER=${HIP_HIPCC_EXECUTABLE}
-         LOG_DOWNLOAD        TRUE
-         LOG_CONFIGURE       TRUE
-         LOG_INSTALL         TRUE
-         BUILD_PROJECT       TRUE
-         UPDATE_DISCONNECT   TRUE
-    )
   find_package(HIPCUB REQUIRED CONFIG PATHS ${ROCPRIM_ROOT})
   endif()
 endif()
