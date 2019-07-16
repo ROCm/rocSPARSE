9bd8e1e8c302dad26f4a9214244aafdc53a275cc
Nico Trost <nico.trost@amd.com>
2019-07-16 11:42:41 +0200

    clang format

--- clients/tests/rocsparse_gtest_main.cpp
+++ clients/tests/rocsparse_gtest_main.cpp
@@ -24,8 +24,8 @@
 #include "utility.hpp"
 
 #include <gtest/gtest.h>
-#include <stdexcept>
 #include <hip/hip_runtime_api.h>
+#include <stdexcept>
 
 /* =====================================================================
       Main function:
