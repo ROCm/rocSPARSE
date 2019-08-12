1bb598d0a37731bfd71b058cdcfc939a2d6ba4da
Nico Trost <nico.trost@amd.com>
2019-08-12 08:37:19 +0200

    adjusted matrix path for CI

--- clients/tests/test_csrgemm.cpp
+++ clients/tests/test_csrgemm.cpp
@@ -136,7 +136,7 @@ Arguments setup_csrgemm_arguments(csrgemm_bin_tuple tup)
     }
 
     // Matrices are stored at the same path in matrices directory
-    arg.filename = std::string(path_exe) + "matrices/" + bin_file;
+    arg.filename = std::string(path_exe) + "../matrices/" + bin_file;
 
     return arg;
 }
