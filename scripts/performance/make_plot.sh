5af9511f5c7f378f74da4b89703d85974a534aad
Nico Trost <nico.trost@amd.com>
2019-11-07 13:49:48 +0100

    few minor fixes for benchmark scripts

--- scripts/performance/make_plot.sh
+++ scripts/performance/make_plot.sh
@@ -30,15 +30,24 @@ for line in "${log[@]}"; do
     # Split line into its strings
     array=($(echo $line | tr " " "\n"))
 
+    # Find GFlop/s slot
+    j=0
+    for slot in "${array[@]}"; do
+        if [ "$slot" = "GFlop/s" ]; then
+            idx=$j
+        fi
+        j=$((j+1))
+    done
+
     # Extract GFlop/s on pattern match
     if [ $found -eq 1 ]; then
         # Add data to tmpfile
-        echo "$i $mat ${array[6]}" >> $tmpfile
+        echo "$i $mat ${array[$idx]}" >> $tmpfile
         i=$((i+1))
     fi
 
     # Check for GFlop/s pattern
-    if [ "${array[6]}" = "GFlop/s" ]; then
+    if [ "${array[$idx]}" = "GFlop/s" ]; then
         found=1
     else
         found=0
@@ -56,6 +65,7 @@ gnuplot -persist <<-EOFMarker
     set termoption noenhanced
     set ylabel "GFlop/s"
     set xrange [-0.5:$i.5]
+    set yrange [0:*]
     set offsets 0.25, 0.25, 10, 0
     set xtics rotate by -45
     set boxwidth 0.5
