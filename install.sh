a256565fb3176c43f7281462129150c91af99c12
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2020-05-13 16:06:22 -0600

    Revert "Single thread compile in install script (#63)" (#69)

--- install.sh
+++ install.sh
@@ -322,7 +322,7 @@ if [[ "${install_dependencies}" == true ]]; then
     printf "\033[32mBuilding \033[33mgoogletest\033[32m from source; installing into \033[33m/usr/local\033[0m\n"
     mkdir -p ${build_dir}/deps && cd ${build_dir}/deps
     ${cmake_executable} ../../deps
-    make
+    make -j$(nproc)
     elevate_if_not_root make install
   popd
 fi
@@ -396,7 +396,7 @@ pushd .
   fi
   check_exit_code
 
-  make install
+  make -j$(nproc) install
   check_exit_code
 
   # #################################################
