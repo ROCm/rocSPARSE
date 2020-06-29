0da3129d2c2ebd04d523df54356147697573f632
Nico <31079890+ntrost57@users.noreply.github.com>
2020-06-29 14:18:28 +0200

    hipclang doc update (#82)
    * hipclang doc update
    
    * doxygen seem to struggle with tabs and spaces
    
    * doc auto version sync

--- docs/run_doc.sh
+++ docs/run_doc.sh
@@ -4,6 +4,11 @@ if [ -d docBin ]; then
     rm -rf docBin
 fi
 
+cur_version=$(sed -n -e "s/^.*VERSION_STRING.* \"\([0-9\.]\{1,\}\).*/\1/p" ../CMakeLists.txt)
+sed -i -e "s/\(PROJECT_NUMBER.*=\)\(.*\)/\1 v${cur_version}/" Doxyfile
+sed -i -e "s/\(version.*=\)\(.*\)/\1 u'${cur_version}'/" source/conf.py
+sed -i -e "s/\(release.*=\)\(.*\)/\1 u'${cur_version}'/" source/conf.py
+
 sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-functions.h > rocsparse-functions.h
 sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-auxiliary.h > rocsparse-auxiliary.h
 sed -i 's/#include "rocsparse-export.h"//g' rocsparse-functions.h
