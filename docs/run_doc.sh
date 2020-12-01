654713b6851902f4d0039e6cb1113b802f173734
Cordell Bloor <Cordell.Bloor@amd.com>
2020-12-01 15:06:51 -0700

    Configure Doxygen to skip copying headers

--- docs/run_doc.sh
+++ docs/run_doc.sh
@@ -8,12 +8,6 @@ cur_version=$(sed -n -e "s/^.*VERSION_STRING.* \"\([0-9\.]\{1,\}\).*/\1/p" ../CM
 sed -i -e "s/\(PROJECT_NUMBER.*=\)\(.*\)/\1 v${cur_version}/" Doxyfile
 sed -i -e "s/\(version.*=\)\(.*\)/\1 u'${cur_version}'/" source/conf.py
 sed -i -e "s/\(release.*=\)\(.*\)/\1 u'${cur_version}'/" source/conf.py
-
-sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-functions.h > rocsparse-functions.h
-sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-auxiliary.h > rocsparse-auxiliary.h
-sed -i 's/#include "rocsparse-export.h"//g' rocsparse-functions.h
-sed -i 's/#include "rocsparse-export.h"//g' rocsparse-auxiliary.h
-cp ../library/include/rocsparse-types.h rocsparse-types.h
 doxygen Doxyfile
 
 cd source
@@ -21,7 +15,3 @@ make clean
 make html
 make latexpdf
 cd ..
-
-rm rocsparse-functions.h
-rm rocsparse-auxiliary.h
-rm rocsparse-types.h
--- docs/run_doxygen.sh
+++ docs/run_doxygen.sh
@@ -7,12 +7,4 @@ fi
 cur_version=$(sed -n -e "s/^.*VERSION_STRING.* \"\([0-9\.]\{1,\}\).*/\1/p" ../CMakeLists.txt)
 # sed -i -e "s/\(PROJECT_NUMBER.*=\)\(.*\)/\1 v${cur_version}/" Doxyfile
 
-sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-functions.h > rocsparse-functions.h
-sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-auxiliary.h > rocsparse-auxiliary.h
-sed -i 's/#include "rocsparse-export.h"//g' rocsparse-functions.h
-sed -i 's/#include "rocsparse-export.h"//g' rocsparse-auxiliary.h
-cp ../library/include/rocsparse-types.h rocsparse-types.h
 doxygen Doxyfile
-rm rocsparse-functions.h
-rm rocsparse-auxiliary.h
-rm rocsparse-types.h
