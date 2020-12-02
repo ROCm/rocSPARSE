e75d9cd7a2201c0f24169e692ae6d98ec2bc3708
Cory Bloor <Cordell.Bloor@amd.com>
2020-12-02 00:06:51 -0700

    Fix Jenkins docs link and minor cleanup (#216)
    * Switch Jenkins docs link from Doxygen to Sphinx
    
    * Configure Doxygen to skip copying headers
    
    * Allow docs scripts to run from anywhere

--- docs/run_doc.sh
+++ docs/run_doc.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-if [ -d docBin ]; then
-    rm -rf docBin
-fi
+set -eu
 
+# Make this directory the PWD
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Update version string
 cur_version=$(sed -n -e "s/^.*VERSION_STRING.* \"\([0-9\.]\{1,\}\).*/\1/p" ../CMakeLists.txt)
 sed -i -e "s/\(PROJECT_NUMBER.*=\)\(.*\)/\1 v${cur_version}/" Doxyfile
 sed -i -e "s/\(version.*=\)\(.*\)/\1 u'${cur_version}'/" source/conf.py
 sed -i -e "s/\(release.*=\)\(.*\)/\1 u'${cur_version}'/" source/conf.py
 
-sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-functions.h > rocsparse-functions.h
-sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-auxiliary.h > rocsparse-auxiliary.h
-sed -i 's/#include "rocsparse-export.h"//g' rocsparse-functions.h
-sed -i 's/#include "rocsparse-export.h"//g' rocsparse-auxiliary.h
-cp ../library/include/rocsparse-types.h rocsparse-types.h
+# Build doxygen info
+rm -rf docBin
 doxygen Doxyfile
 
+# Build sphinx docs
 cd source
 make clean
 make html
 make latexpdf
-cd ..
-
-rm rocsparse-functions.h
-rm rocsparse-auxiliary.h
-rm rocsparse-types.h
--- docs/run_doxygen.sh
+++ docs/run_doxygen.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-if [ -d docBin ]; then
-    rm -rf docBin
-fi
+set -eu
 
-cur_version=$(sed -n -e "s/^.*VERSION_STRING.* \"\([0-9\.]\{1,\}\).*/\1/p" ../CMakeLists.txt)
+# Make this directory the PWD
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Update version string
+# cur_version=$(sed -n -e "s/^.*VERSION_STRING.* \"\([0-9\.]\{1,\}\).*/\1/p" ../CMakeLists.txt)
 # sed -i -e "s/\(PROJECT_NUMBER.*=\)\(.*\)/\1 v${cur_version}/" Doxyfile
 
-sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-functions.h > rocsparse-functions.h
-sed -e 's/ROCSPARSE_EXPORT//g' ../library/include/rocsparse-auxiliary.h > rocsparse-auxiliary.h
-sed -i 's/#include "rocsparse-export.h"//g' rocsparse-functions.h
-sed -i 's/#include "rocsparse-export.h"//g' rocsparse-auxiliary.h
-cp ../library/include/rocsparse-types.h rocsparse-types.h
+# Build the doxygen info
+rm -rf docBin
 doxygen Doxyfile
-rm rocsparse-functions.h
-rm rocsparse-auxiliary.h
-rm rocsparse-types.h
