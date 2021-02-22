7b177b0c376e3da4df8f1517b4be814ef7253925
Yvan Mokwinski <yvan.mokwinski@amd.com>
2021-02-22 12:26:10 -0700

    code coverage config in jenkins (#186)

---
+++ .jenkins/codecov.groovy
@@ -0,0 +1,82 @@
+#!/usr/bin/env groovy
+// This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
+@Library('rocJenkins@pong') _
+
+// This is file for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+import com.amd.project.*
+import com.amd.docker.*
+import java.nio.file.Path
+
+def runCI =
+{
+    nodeDetails, jobName->
+    def prj  = new rocProject('rocSPARSE', 'CodeCov')
+
+    // customize for project
+    prj.paths.build_command = './install.sh -kpc'
+    prj.libraryDependencies = ['rocPRIM']
+
+    // Define test architectures, optional rocm version argument is available
+    def nodes = new dockerNodes(nodeDetails, jobName, prj)
+
+    def commonGroovy
+
+    boolean formatCheck = false
+
+    def compileCommand =
+    {
+        platform, project->
+
+        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
+        commonGroovy.runCompileCommand(platform, project, jobName)
+    }
+
+    def testCommand =
+    {
+        platform, project->
+
+        def gfilter = "*pre_checkin*"
+        commonGroovy.runCoverageCommand(platform, project, gfilter, "release-debug")
+    }
+
+    buildProject(prj, formatCheck, nodes.dockerArray, compileCommand, testCommand, null)
+}
+
+ci: {
+    String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
+
+    def propertyList = ["compute-rocm-dkms-no-npi":[pipelineTriggers([cron('0 1 * * 6')])],
+                        "compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 1 * * 6')])] ]
+    propertyList = auxiliary.appendPropertyList(propertyList)
+
+    def jobNameList = ["compute-rocm-dkms-no-npi":([ubuntu18:['gfx900']]),
+                       "compute-rocm-dkms-no-npi-hipclang":([ubuntu18:['gfx900']])]
+    jobNameList = auxiliary.appendJobNameList(jobNameList)
+
+    propertyList.each
+    {
+        jobName, property->
+        if (urlJobName == jobName)
+            properties(auxiliary.addCommonProperties(property))
+    }
+
+    jobNameList.each
+    {
+        jobName, nodeDetails->
+        if (urlJobName == jobName)
+            stage(jobName) {
+                runCI(nodeDetails, jobName)
+            }
+    }
+
+    // For url job names that are not listed by the jobNameList i.e. compute-rocm-dkms-no-npi-1901
+    if(!jobNameList.keySet().contains(urlJobName))
+    {
+        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
+        stage(urlJobName) {
+            runCI([ubuntu18:['gfx900']], urlJobName)
+        }
+    }
+}
--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -28,13 +28,14 @@ def runCompileCommand(platform, project, jobName, boolean sameOrg=false)
     platform.runCommand(this, command)
 }
 
-def runTestCommand (platform, project, gfilter)
+
+def runTestCommand (platform, project, gfilter, String dirmode = "release")
 {
     String sudo = auxiliary.sudo(platform.jenkinsLabel)
 
     def command = """#!/usr/bin/env bash
                 set -x
-                cd ${project.paths.project_build_prefix}/build/release/clients/staging
+                cd ${project.paths.project_build_prefix}/build/${dirmode}/clients/staging
                 ${sudo} LD_LIBRARY_PATH=/opt/rocm/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./rocsparse-test --gtest_output=xml --gtest_color=yes --gtest_filter=${gfilter}-*known_bug*
             """
 
@@ -42,7 +43,28 @@ def runTestCommand (platform, project, gfilter)
     junit "${project.paths.project_build_prefix}/build/release/clients/staging/*.xml"
 }
 
-def runPackageCommand(platform, project)
+def runCoverageCommand (platform, project, gfilter, String dirmode = "release")
+{
+    String sudo = auxiliary.sudo(platform.jenkinsLabel)
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}/build/${dirmode}
+                ${sudo} LD_LIBRARY_PATH=/opt/rocm/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG make coverage_cleanup coverage GTEST_FILTER=${gfilter}-*known_bug*
+            """
+	    
+    platform.runCommand(this, command)
+
+    publishHTML([allowMissing: false,
+                alwaysLinkToLastBuild: false,
+                keepAll: false,
+                reportDir: "${project.paths.project_build_prefix}/build/${dirmode}/lcoverage",
+                reportFiles: "index.html",
+                reportName: "Code coverage report",
+                reportTitles: "Code coverage report"])
+}
+
+def runPackageCommand(platform, project, String dirmode = "release")
 {
     String sudo = auxiliary.sudo(platform.jenkinsLabel)
 
@@ -52,7 +74,7 @@ def runPackageCommand(platform, project)
     {
         command = """
                 set -x
-                cd ${project.paths.project_build_prefix}/build/release
+                cd ${project.paths.project_build_prefix}/build/${dirmode}
                 ${sudo} make package
                 ${sudo} mkdir -p package
                 ${sudo} mv *.rpm package/
@@ -60,7 +82,7 @@ def runPackageCommand(platform, project)
             """
 
         platform.runCommand(this, command)
-        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.rpm""")
+        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/${dirmode}/package/*.rpm""")
     }
     else if(platform.jenkinsLabel.contains('hip-clang'))
     {
@@ -70,7 +92,7 @@ def runPackageCommand(platform, project)
     {
         command = """
                 set -x
-                cd ${project.paths.project_build_prefix}/build/release
+                cd ${project.paths.project_build_prefix}/build/${dirmode}
                 make package
                 make package_clients
                 mkdir -p package
@@ -79,7 +101,7 @@ def runPackageCommand(platform, project)
             """
 
         platform.runCommand(this, command)
-        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.deb""")
+        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/${dirmode}/package/*.deb""")
     }
 }
 
