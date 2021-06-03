5ccd66293b4fbe8832d724b067b560c5590c71a0
Yvan Mokwinski <yvan.mokwinski@amd.com>
2021-06-04 01:37:20 +0200

    Coverage modif (#230)
    * small modifications for standardization
    
    * changing depends
    
    * remove -g options since it's already added by the allowed modes
    
    [ROCm/rocSPARSEcommit: f3ca1b67c13394e97fb37d7d3a2ab607ec0245c6]

--- .jenkins/codecov.groovy
+++ .jenkins/codecov.groovy
@@ -15,7 +15,7 @@ def runCI =
     def prj  = new rocProject('rocSPARSE', 'CodeCov')
 
     // customize for project
-    prj.paths.build_command = './install.sh --matrices-dir-install ${JENKINS_HOME_DIR}/rocsparse_matrices && ./install.sh -kpc --matrices-dir ${JENKINS_HOME_DIR}/rocsparse_matrices'
+    prj.paths.build_command = './install.sh --matrices-dir-install ${JENKINS_HOME_DIR}/rocsparse_matrices && ./install.sh -kc --codecoverage --matrices-dir ${JENKINS_HOME_DIR}/rocsparse_matrices'
     prj.libraryDependencies = ['rocPRIM']
     prj.defaults.ccache = false
 
