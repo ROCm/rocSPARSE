8b82f78e5bdc35842c1adf720e679467b9500da5
James Sandham <33790278+jsandham@users.noreply.github.com>
2022-05-04 09:24:02 -0600

    fix bugs and warnings (#336)
    Co-authored-by: jsandham <james.sandham@amd.com>
    
    [ROCm/rocSPARSEcommit: d24611331aa78088df7ad999e7b7d96269d24075]

--- clients/common/rocsparse_matrix_factory_laplace3d.cpp
+++ clients/common/rocsparse_matrix_factory_laplace3d.cpp
@@ -95,6 +95,11 @@ void rocsparse_matrix_factory_laplace3d<T, I, J>::init_csr(std::vector<I>&      
     {
         rocsparse_matrix_utils::host_csrunsort<T, I, J>(
             csr_row_ptr.data(), csr_col_ind.data(), M, base);
+        break;
+    }
+    case rocsparse_storage_mode_sorted:
+    {
+        break;
     }
     }
 }
@@ -159,6 +164,11 @@ void rocsparse_matrix_factory_laplace3d<T, I, J>::init_coo(std::vector<I>&      
     {
         rocsparse_matrix_utils::host_coounsort<T, I>(
             coo_row_ind.data(), coo_col_ind.data(), M, nnz, base);
+        break;
+    }
+    case rocsparse_storage_mode_sorted:
+    {
+        break;
     }
     }
 }
@@ -197,6 +207,11 @@ void rocsparse_matrix_factory_laplace3d<T, I, J>::init_gebsr(std::vector<I>&    
     {
         rocsparse_matrix_utils::host_gebsrunsort<T, I, J>(
             bsr_row_ptr.data(), bsr_col_ind.data(), Mb, base);
+        break;
+    }
+    case rocsparse_storage_mode_sorted:
+    {
+        break;
     }
     }
 }
