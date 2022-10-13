13be4d545d034d61ca4f0e70c664764019b56fd1
James Sandham <33790278+jsandham@users.noreply.github.com>
2022-10-13 10:44:26 -0600

    Performance plotting script fixes (#406)
    * fixes to performance python plotting scripts
    
    * plotting fixes
    
    * fixes to PR
    
    Co-authored-by: jsandham <james.sandham@amd.com>

--- scripts/rocsparse-bench-plot.py
+++ scripts/rocsparse-bench-plot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # ########################################################################
-# Copyright (C) 2021 Advanced Micro Devices, Inc. All rights Reserved.
+# Copyright (C) 2021-2022 Advanced Micro Devices, Inc. All rights Reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -41,7 +41,7 @@ import rocsparse_bench_gnuplot_helper
 # arg plot: "all", "gflops", "time", "bandwidth"
 #
 #
-def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug = False):
+def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug = False,linear=False):
 
     datafile = open(obasename + ".dat", "w+")
     len_xargs = len(xargs)
@@ -49,16 +49,31 @@ def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug =
         for ixarg  in range(len_xargs):
             isample = iplot * len_xargs + ixarg
             tg = results[isample]["timing"]
+
+            min_allowed_yvalue = 0.0
+            if not linear:
+                min_allowed_yvalue = 1.0
+
+            time0 = max(float(tg["time"][0]), min_allowed_yvalue)
+            time1 = max(float(tg["time"][1]), min_allowed_yvalue)
+            time2 = max(float(tg["time"][2]), min_allowed_yvalue)
+            flops0 = max(float(tg["flops"][0]), min_allowed_yvalue)
+            flops1 = max(float(tg["flops"][1]), min_allowed_yvalue)
+            flops2 = max(float(tg["flops"][2]), min_allowed_yvalue)
+            bandwidth0 = max(float(tg["bandwidth"][0]), min_allowed_yvalue)
+            bandwidth1 = max(float(tg["bandwidth"][1]), min_allowed_yvalue)
+            bandwidth2 = max(float(tg["bandwidth"][2]), min_allowed_yvalue)
+
             datafile.write(os.path.basename(os.path.splitext(xargs[ixarg])[0]) + " " +
-                           tg["time"][0] + " " +
-                           tg["time"][1] + " " +
-                           tg["time"][2] + " " +
-                           tg["flops"][0] + " " +
-                           tg["flops"][1] + " " +
-                           tg["flops"][2] + " " +
-                           tg["bandwidth"][0] + " " +
-                           tg["bandwidth"][1] + " "+
-                           tg["bandwidth"][2] + "\n")
+                           str(time0) + " " +
+                           str(time1) + " " +
+                           str(time2) + " " +
+                           str(flops0) + " " +
+                           str(flops1) + " " +
+                           str(flops2) + " " +
+                           str(bandwidth0) + " " +
+                           str(bandwidth1) + " "+
+                           str(bandwidth2) + "\n")
         datafile.write("\n")
         datafile.write("\n")
     datafile.close();
@@ -80,7 +95,8 @@ def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug =
                                                  [-0.5,len_xargs + 0.5],
                                                  "milliseconds",
                                                  2,3,4,
-                                                 yargs)
+                                                 yargs,
+                                                 linear)
     elif plot == "gflops":
         rocsparse_bench_gnuplot_helper.histogram(cmdfile,
                                                  obasename + filename_extension,
@@ -90,7 +106,8 @@ def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug =
                                                  [-0.5,len_xargs + 0.5],
                                                  "GFlops",
                                                  5,6,7,
-                                                 yargs)
+                                                 yargs,
+                                                 linear)
     elif plot == "bandwidth":
         rocsparse_bench_gnuplot_helper.histogram(cmdfile,
                                                  obasename + filename_extension,
@@ -100,7 +117,8 @@ def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug =
                                                  [-0.5,len_xargs + 0.5],
                                                  "GBytes/s",
                                                  8,9,10,
-                                                 yargs)
+                                                 yargs,
+                                                 linear)
     elif plot == "all":
         rocsparse_bench_gnuplot_helper.histogram(cmdfile,
                                                  obasename + "_msec"+ filename_extension,
@@ -110,7 +128,8 @@ def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug =
                                                  [-0.5,len_xargs + 0.5],
                                                  "milliseconds",
                                                  2,3,4,
-                                                 yargs)
+                                                 yargs,
+                                                 linear)
 
 
         rocsparse_bench_gnuplot_helper.histogram(cmdfile,
@@ -121,7 +140,8 @@ def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug =
                                                  [-0.5,len_xargs + 0.5],
                                                  "GFlops",
                                                  5,6,7,
-                                                 yargs)
+                                                 yargs,
+                                                 linear)
 
         rocsparse_bench_gnuplot_helper.histogram(cmdfile,
                                                  obasename + "_bandwidth"+ filename_extension,
@@ -131,7 +151,8 @@ def export_gnuplot(plot, obasename,xargs, yargs, results,verbose = False,debug =
                                                  [-0.5,len_xargs + 0.5],
                                                  "GBytes/s",
                                                  8,9,10,
-                                                 yargs)
+                                                 yargs,
+                                                 linear)
     else:
         print("//rocsparse-bench-plot::error invalid plot keyword '"+plot+"', must be 'all' (default), 'time', 'gflops' or 'bandwidth' ")
         exit(1)
@@ -155,11 +176,14 @@ def main():
     parser.add_argument('-p', '--plot',    required=False, default = 'all')
     parser.add_argument('-v', '--verbose',         required=False, default = False, action = "store_true")
     parser.add_argument('-d', '--debug',         required=False, default = False, action = "store_true")
+    parser.add_argument('-l', '--linear',         required=False, default = False, action = "store_true")
     user_args, unknown_args = parser.parse_known_args()
     verbose=user_args.verbose
     debug=user_args.debug
     obasename = user_args.obasename
     plot = user_args.plot
+    linear = user_args.linear
+
     if len(unknown_args) > 1:
         print('expecting only one input file.')
     with open(unknown_args[0],"r") as f:
@@ -176,7 +200,7 @@ def main():
         print('//rocsparse-bench-plot')
         print('//rocsparse-bench-plot  - file : \'' + unknown_args[0] + '\'')
 
-    export_gnuplot(plot, obasename, xargs,yargs, results, verbose,debug)
+    export_gnuplot(plot, obasename, xargs,yargs, results, verbose, debug, linear)
 
 if __name__ == "__main__":
     main()
