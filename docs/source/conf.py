ce31b692b31a1567ee5e9ca5c86a42bb1f69c731
Cory Bloor <Cordell.Bloor@amd.com>
2022-12-14 02:36:09 -0700

    Update docs build and style (#458)
    * Drop CSS overrides from docs
    
    * Fix Sphinx language error
    
    * Enable Sphinx warnings-as-errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,7 +73,7 @@ release = u'1.19.6'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -110,13 +110,7 @@ else:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
