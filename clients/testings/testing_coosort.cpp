7a09f6c6700312830b3e546adf4560976b30edd3
Angelo Gonzales <48339672+Angeloo01@users.noreply.github.com>
2023-02-08 10:20:27 -0700

    fix coosort timing (#498)
    [ROCm/rocSPARSEcommit: 990423b20cd212eefed2b5392bea1aae08c26c3c]

--- clients/testings/testing_coosort.cpp
+++ clients/testings/testing_coosort.cpp
@@ -1,6 +1,6 @@
 /*! \file */
 /* ************************************************************************
- * Copyright (C) 2019-2022 Advanced Micro Devices, Inc. All rights Reserved.
+ * Copyright (C) 2019-2023 Advanced Micro Devices, Inc. All rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -221,6 +221,8 @@ void testing_coosort(const Arguments& arg)
                                                                   dbuffer));
             }
         }
+
+        gpu_time_used = (get_time_us() - gpu_time_used) / number_hot_calls;
 
         double gbyte_count = coosort_gbyte_count<T>(nnz, permute);
         double gpu_gbyte   = get_gpu_gbyte(gpu_time_used, gbyte_count);
