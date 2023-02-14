ea47aaec88d56c77fa8f04c0286e64262bb657ed
Yvan Mokwinski <yvan.mokwinski@gmail.com>
2023-02-14 11:39:58 -0700

    Fixing memstat output json file and fixing a csrsv memory leak  (#507)
    * Fixing memstat output json file and fixing a memory leak from trm_analysis
    
    * clang ...

--- library/src/rocsparse_memstat.cpp
+++ library/src/rocsparse_memstat.cpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2022 Advanced Micro Devices, Inc. All rights Reserved.
+ * Copyright (C) 2022-2023 Advanced Micro Devices, Inc. All rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -900,6 +900,7 @@ void memstat::flush_report(std::ostream& out, bool finalize) const
             }
             else
             {
+                out << "\"leaks\": [";
                 bool first = true;
                 for(auto f : m_map)
                 {
