749beedd3c1bb83f5de21c4c46d35622751cc8db
Yvan Mokwinski <yvan.mokwinski@gmail.com>
2024-01-16 11:16:21 -0700

    closing FILE (#710)

--- clients/common/rocsparse_importer_mlcsr.cpp
+++ clients/common/rocsparse_importer_mlcsr.cpp
@@ -1,6 +1,6 @@
 /*! \file */
 /* ************************************************************************
- * Copyright (C) 2023 Advanced Micro Devices, Inc. All rights Reserved.
+ * Copyright (C) 2023-2024 Advanced Micro Devices, Inc. All rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,14 @@
  * ************************************************************************ */
 #include "rocsparse_importer_mlcsr.hpp"
 #include <stdio.h>
+
+rocsparse_importer_mlcsr::~rocsparse_importer_mlcsr()
+{
+    if(this->m_f != nullptr)
+    {
+        fclose(this->m_f);
+    }
+}
 
 rocsparse_importer_mlcsr::rocsparse_importer_mlcsr(const std::string& filename_)
     : m_filename(filename_)
