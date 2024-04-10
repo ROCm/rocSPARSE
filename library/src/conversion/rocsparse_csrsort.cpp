2e716660744c37d0f4f0206ee1c30bdb716ead3b
James Sandham <33790278+jsandham@users.noreply.github.com>
2024-04-09 20:34:28 -0400

    Revert "Fix rocprim failures (#776)" (#778)
    This reverts commit 4cc21d9a2c946266ecbe7f42e2b8201216134ec4.
    
    Co-authored-by: root <root@banff-cyxtera-s77-2.ctr.dcgpu>

--- library/src/conversion/rocsparse_csrsort.cpp
+++ library/src/conversion/rocsparse_csrsort.cpp
@@ -213,7 +213,8 @@ try
 
         if(avg_row_nnz < 64)
         {
-            using config = rocprim::segmented_radix_sort_config<6, 5, 64, 1>;
+            using config
+                = rocprim::segmented_radix_sort_config<6, 5, rocprim::kernel_config<64, 1>>;
             RETURN_IF_HIP_ERROR(rocprim::segmented_radix_sort_pairs<config>(tmp_rocprim,
                                                                             size,
                                                                             keys,
@@ -228,7 +229,8 @@ try
         }
         else if(avg_row_nnz < 128)
         {
-            using config = rocprim::segmented_radix_sort_config<6, 5, 64, 2>;
+            using config
+                = rocprim::segmented_radix_sort_config<6, 5, rocprim::kernel_config<64, 2>>;
             RETURN_IF_HIP_ERROR(rocprim::segmented_radix_sort_pairs<config>(tmp_rocprim,
                                                                             size,
                                                                             keys,
@@ -243,7 +245,8 @@ try
         }
         else if(avg_row_nnz < 256)
         {
-            using config = rocprim::segmented_radix_sort_config<6, 5, 64, 4>;
+            using config
+                = rocprim::segmented_radix_sort_config<6, 5, rocprim::kernel_config<64, 4>>;
             RETURN_IF_HIP_ERROR(rocprim::segmented_radix_sort_pairs<config>(tmp_rocprim,
                                                                             size,
                                                                             keys,
@@ -297,19 +300,22 @@ try
 
         if(avg_row_nnz < 64)
         {
-            using config = rocprim::segmented_radix_sort_config<6, 5, 64, 1>;
+            using config
+                = rocprim::segmented_radix_sort_config<6, 5, rocprim::kernel_config<64, 1>>;
             RETURN_IF_HIP_ERROR(rocprim::segmented_radix_sort_keys<config>(
                 tmp_rocprim, size, keys, nnz, m, offsets, offsets + 1, startbit, endbit, stream));
         }
         else if(avg_row_nnz < 128)
         {
-            using config = rocprim::segmented_radix_sort_config<6, 5, 64, 2>;
+            using config
+                = rocprim::segmented_radix_sort_config<6, 5, rocprim::kernel_config<64, 2>>;
             RETURN_IF_HIP_ERROR(rocprim::segmented_radix_sort_keys<config>(
                 tmp_rocprim, size, keys, nnz, m, offsets, offsets + 1, startbit, endbit, stream));
         }
         else if(avg_row_nnz < 256)
         {
-            using config = rocprim::segmented_radix_sort_config<6, 5, 64, 4>;
+            using config
+                = rocprim::segmented_radix_sort_config<6, 5, rocprim::kernel_config<64, 4>>;
             RETURN_IF_HIP_ERROR(rocprim::segmented_radix_sort_keys<config>(
                 tmp_rocprim, size, keys, nnz, m, offsets, offsets + 1, startbit, endbit, stream));
         }
