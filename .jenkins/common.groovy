df97207af1e82f34fd2a6c33bdb2ac60f4671df5
amd-garydeng <garydeng@amd.com>
2024-09-02 20:43:24 -0600

    update dependencies in .jenkins (#843)
    * update dependencies in .jenkins
    
    * Grab dependencies from main/precheckin
    
    [ROCm/rocSPARSEcommit: 7c94845ab491a8c56b59735eda523b75bc551760]

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -1,7 +1,7 @@
 // This file is for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
 
-def runCompileCommand(platform, project, jobName, boolean sameOrg=true)
+def runCompileCommand(platform, project, jobName, boolean sameOrg=false)
 {
     project.paths.construct_build_prefix()
 
