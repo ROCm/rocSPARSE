9775f0fe5a84b8b01108cb18da4c75b941c7ad12
jsandham <jsandham@amd.com>
2025-06-03 14:41:15 +0000

    PR fixes

--- install.sh
+++ install.sh
@@ -308,7 +308,6 @@ build_address_sanitizer=false
 build_memstat=false
 build_rocsparse_ILP64=false
 build_with_rocblas=true
-build_with_roctx=true
 build_with_offload_compress=true
 matrices_dir=
 matrices_dir_install=
@@ -388,9 +387,6 @@ while true; do
             shift ;;
         --no-rocblas)
             build_with_rocblas=false
-            shift ;;
-        --no-roctx)
-            build_with_roctx=false
             shift ;;
         -k|--relwithdebinfo)
             build_release=false
@@ -593,13 +589,6 @@ pushd .
     cmake_common_options+=("-DBUILD_WITH_ROCBLAS=ON")
   else
     cmake_common_options+=("-DBUILD_WITH_ROCBLAS=OFF")
-  fi
-
-  # roctx
-  if [[ "${build_with_roctx}" == true ]]; then
-    cmake_common_options+=("-DBUILD_WITH_ROCTX=ON")
-  else
-    cmake_common_options+=("-DBUILD_WITH_ROCTX=OFF")
   fi
 
   # freorg backward compatible support enable
