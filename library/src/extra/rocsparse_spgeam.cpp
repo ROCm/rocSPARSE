5e63e7393d7552134afe1f819f0485b4f09f4792
jsandham <jsandham@amd.com>
2025-06-04 17:57:01 +0000

    Fix spgeam extra test that was failing

--- library/src/extra/rocsparse_spgeam.cpp
+++ library/src/extra/rocsparse_spgeam.cpp
@@ -391,6 +391,16 @@ namespace rocsparse
                     temp_buffer,
                     true));
 
+                if(mat_C != nullptr && mat_C->row_type == rocsparse_indextype_i32)
+                {
+                    // Temporary hack to handle the fact that we pass mat_C->nnz as an int64_t* but internally in order to match
+                    // the legacy API we handle nnz_C using int32_t* when csr_row_ptr_C is int32_t*
+                    int32_t* int32_nnz_C;
+                    int32_nnz_C = reinterpret_cast<int32_t*>(&mat_C->nnz);
+
+                    mat_C->nnz = *int32_nnz_C;
+                }
+
                 return rocsparse_status_success;
             }
             case rocsparse_format_bsr:
