d37ea1751bf0991bfd9f2bc6ef9f1ea27c91ee15
James Sandham <33790278+jsandham@users.noreply.github.com>
2026-04-09 14:11:32 +0000

    [rocm-libraries] ROCm/rocm-libraries#6108 (commit f0c725d)
    [rocsparse] Fix building with cmake>=4.0.0
    
    ## Motivation
    
    Fix building rocsparse with cmake version 4.0.0. or greater.
    
    ## Technical Details
    
    Remove use of `cmake_policy(SET CMP0037 OLD)` policy as it is not
    supported in cmake >=4.0.0

--- install.sh
+++ install.sh
@@ -523,7 +523,7 @@ if [[ "${install_dependencies}" == true ]]; then
     mkdir -p ${build_dir}/deps && cd ${build_dir}/deps
     ${cmake_executable} ../../deps
     make -j$(nproc)
-    elevate_if_not_root make install
+    elevate_if_not_root make install_deps
   popd
 fi
 
